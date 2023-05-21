182e853beff15391bafd86a751b68b661d9fbcbd
FarahaniMehrshad <mehrshad.farahani.jafari@gmail.com>
2023-05-21 10:23:20 +0330

    Add files.

---
+++ src/Application.cpp
@@ -0,0 +1,110 @@
+#include "GL/glew.h"
+#include <irrKlang.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "Shader.h"
+#include "GLError.h"
+#include "Rect.h"
+#include "Pong.h"
+#include "Interface.h"
+#include "freeglut.h"
+#include "glut.h"
+#include "Render.h"
+
+void OpenGLInit()
+{
+	GLuint buffer{};
+	glGenBuffers(1, &buffer);
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+
+	ShaderProgramSource shaders{ParseShader("res/shaders/Basic.shaders")};
+
+	GLuint vertexShader{glCreateShader(GL_VERTEX_SHADER)};
+	GLuint fragmentShader{glCreateShader(GL_FRAGMENT_SHADER)};
+
+	const char* vsrc = shaders.VertexSource.c_str();
+	const char* fsrc = shaders.FragmentSource.c_str();
+
+	glShaderSource(vertexShader, 1, &vsrc, nullptr);
+	glShaderSource(fragmentShader, 1, &fsrc, nullptr);
+	glCompileShader(vertexShader);
+
+	GLint compiled;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled)
+	{
+		GLint logSize{};
+		glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &logSize);
+		char* logMsg = new char[logSize];
+		glGetShaderInfoLog(vertexShader, logSize, NULL, logMsg);
+		std::cout << logMsg << std::endl;
+		delete[] logMsg;
+		std::cout << "ERROR!!!" << std::endl;
+	}
+
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled)
+	{
+		GLint logSize{};
+		glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &logSize);
+		char* logMsg = new char[logSize];
+		glGetShaderInfoLog(fragmentShader, logSize, NULL, logMsg);
+		std::cout << logMsg << std::endl;
+		delete[] logMsg;
+		std::cout << "ERROR!!!" << std::endl;
+	}
+
+	GLuint program{glCreateProgram()};
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	glUseProgram(program);
+
+	GLint location{ glGetAttribLocation(program, "position") };
+
+	GLCall(glEnableVertexAttribArray(location));
+
+	GLCall(glVertexAttribPointer(location, 3, GL_FLOAT, GL_FALSE, 0, 0));
+}
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitContextVersion(3, 0);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+
+	int window{ glutCreateWindow("PONG GAME") };
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	OpenGLInit();
+
+	renderTexts();
+
+	glutIdleFunc(display);
+
+	glutKeyboardFunc(keyboardHandler);
+	glutKeyboardUpFunc(keyboardUpHandler);
+
+	glutDisplayFunc(display);
+
+	using namespace irrklang;
+
+	ISoundEngine* SoundEngine = createIrrKlangDevice();
+
+	SoundEngine->play2D("pong.mp3", true);
+
+	glutMainLoop();
+
+	delete SoundEngine;
+
+	return 0;
+}
---
+++ src/Shader.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				//set mode to vertex
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				//set mode to fragment
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
